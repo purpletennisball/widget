82afb8d069cbf8d12e536672512fd7a526176db7
purpletennisball <212221402+purpletennisball@users.noreply.github.com>
2026-08-25 00:50:27 -0400

    fix CalendarService

--- src/CalendarService.ts
+++ src/CalendarService.ts
@@ -1,25 +1,14 @@
+import type { App } from "obsidian";
+import type { PTBWidgetPluginSettings } from "./settings";
+
 export class CalendarService {
 	constructor(private app: App, private settings: PTBWidgetPluginSettings) {}
 
-	getDaysInMonth(year, month) {
+	getDaysInMonth(year: number, month: number) {
     	return new Date(year, month + 1, 0).getDate();
 	}
 
 	getDaysInMonthByDate(date: Date) {
-    	return new Date(date.getYear(), date.getMonth() + 1, 0).getDate();
+    	return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 	}
-
-	getFirstDayForGridMonth(month) {
-		let startDay = 0 // 0 is Sun, 1 is Mon
-
-		let date = new Date();
-		let startDateGrid = date
-
-		let subtractDays = startDateGrid.getDay() - startDay
-		startDateGrid.setDate(startDateGrid.getDate() - subtractDays);
-
-
-	}
-
-	
 }
