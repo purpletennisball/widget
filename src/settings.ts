51d69d437892a3de4e584cf324f7ecea2bf480f0
purpletennisball <212221402+purpletennisball@users.noreply.github.com>
2026-08-25 05:30:29 -0400

    use new settings api

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,4 @@
-import { App, PluginSettingTab, Setting } from 'obsidian';
+import { App, PluginSettingTab, type SettingDefinitionItem } from 'obsidian';
 import { writable } from 'svelte/store';
 import PTBWidgetPlugin from './main';
 
@@ -24,53 +24,48 @@ export class SettingTab extends PluginSettingTab {
 		this.plugin = plugin;
 	}
 
-	display(): void {
-		const { containerEl } = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-            .setName('Weight')
-            .setHeading();
-
-		new Setting(containerEl)
-			.setName('Date')
-			.setDesc("Where to get the date from. Defaults to the creation date of the note.")
-			.addDropdown((dropdown) => {
-				dropdown.addOption('creation', 'Creation date');
-				dropdown.addOption('custom-property', 'Custom property');
-				dropdown.setValue(this.plugin.settings.dateSetting);
-				dropdown.onChange(async (value) => {
-					this.plugin.settings.dateSetting = value;
-					await this.plugin.saveSettings();
-				});
-			});
-
-		new Setting(containerEl)
-			.setName('Date property')
-			.setDesc("The custom property to use for the date.")
-			.addText((text) => {
-				text.setPlaceholder('Enter the date property name');
-				text.setValue(this.plugin.settings.dateProperty);
-				text.onChange(async (value) => {
-					this.plugin.settings.dateProperty = value;
-					await this.plugin.saveSettings();
-				});
-			});
-
-		new Setting(containerEl)
-			.setName('Highlight gains/losses')
-			.setDesc("Highlight colors for gains or losses.")
-			.addDropdown((dropdown) => {
-				dropdown.addOption('gainIsBad', 'Gain is red, loss is green');
-				dropdown.addOption('lossIsBad', 'Gain is green, loss is red');
-				dropdown.addOption('none', 'No highlighting');
-				dropdown.setValue(this.plugin.settings.highlightSetting);
-				dropdown.onChange(async (value) => {
-					this.plugin.settings.highlightSetting = value;
-					highlightSetting.set(value);
-					await this.plugin.saveSettings();
-				});
-			});
+	getSettingDefinitions(): SettingDefinitionItem[] {
+		return [
+			{
+				type: 'group',
+				heading: 'Weight',
+				items: [
+					{
+						name: 'Date',
+						desc: 'Where to get the date from. Defaults to the creation date of the note.',
+						control: {
+							type: 'dropdown',
+							key: 'dateSetting',
+							options: {
+								creation: 'Creation date',
+								'custom-property': 'Custom property',
+							},
+						},
+					},
+					{
+						name: 'Date property',
+						desc: 'The custom property to use for the date.',
+						control: {
+							type: 'text',
+							key: 'dateProperty',
+							placeholder: 'Enter the date property name',
+						},
+					},
+					{
+						name: 'Highlight gains/losses',
+						desc: 'Highlight colors for gains or losses.',
+						control: {
+							type: 'dropdown',
+							key: 'highlightSetting',
+							options: {
+								gainIsBad: 'Gain is red, loss is green',
+								lossIsBad: 'Gain is green, loss is red',
+								none: 'No highlighting',
+							},
+						},
+					},
+				],
+			},
+		];
 	}
 }
